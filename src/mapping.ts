416f9b60cc692bd57abbfbfb246e903cb47e6520
James Oshomah <Jamiebones2000@yahoo.co.uk>
2022-07-21 14:12:14 +0100

    made the subgraph to query for tokens used

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,67 +1,50 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  DaiToken,
-  Approval,
-  LogNote,
-  Transfer
-} from "../generated/DaiToken/DaiToken"
-import { ExampleEntity } from "../generated/schema"
+  DaiToken as DaiTokenContract,
+  Transfer as TransferEvent,
+} from "../generated/DaiToken/DaiToken";
+import { User, TokenHeld } from "../generated/schema";
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleTransfer(event: TransferEvent): void {
+  let receiverUser = User.load(event.params.dst.toHex());
+  let sourceUser = User.load(event.params.src.toHex());
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  //load the contract and find the balance as at that point int time
+  let Contract = DaiTokenContract.bind(event.address);
+  let receiverBalance = Contract.balanceOf(event.params.dst);
+  let sourceBalance = Contract.balanceOf(event.params.src);
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  if (!receiverUser) {
+    //create a new receiver here
+    receiverUser = new User(event.params.dst.toHex());
+    receiverUser.save();
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  if (!sourceUser) {
+    sourceUser = new User(event.params.src.toHex());
+    sourceUser.save();
+  }
 
-  // Entity fields can be set based on event parameters
-  entity.src = event.params.src
-  entity.guy = event.params.guy
+  let timeStamp = event.block.timestamp;
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+  let sourceTokenHeld = new TokenHeld(
+    event.transaction.from.toHex().concat(event.params.src.toHex())
+  );
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+  sourceTokenHeld.amount = event.params.wad;
+  sourceTokenHeld.currentValue = sourceBalance;
+  sourceTokenHeld.receiverAccount = event.params.dst.toHex();
+  sourceTokenHeld.sourceAccount = event.params.src.toHex();
+  sourceTokenHeld.timeStamp = timeStamp;
+  sourceTokenHeld.save();
 
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DOMAIN_SEPARATOR(...)
-  // - contract.PERMIT_TYPEHASH(...)
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.decimals(...)
-  // - contract.name(...)
-  // - contract.nonces(...)
-  // - contract.symbol(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
-  // - contract.version(...)
-  // - contract.wards(...)
+  let receiverTokenHeld = new TokenHeld(
+    event.transaction.from.toHex().concat(event.params.dst.toHex())
+  );
+
+  receiverTokenHeld.amount = event.params.wad;
+  receiverTokenHeld.currentValue = receiverBalance;
+  receiverTokenHeld.receiverAccount = event.params.dst.toHex();
+  receiverTokenHeld.sourceAccount = event.params.src.toHex();
+  receiverTokenHeld.timeStamp = timeStamp;
+  receiverTokenHeld.save();
 }
-
-export function handleLogNote(event: LogNote): void {}
-
-export function handleTransfer(event: Transfer): void {}
